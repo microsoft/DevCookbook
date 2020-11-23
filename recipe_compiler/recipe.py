d571d43ea6c3aac92f2749ea15cc008602159a9a
theckert <thomas.james.eckert@gmail.com>
2020-11-22 18:51:47 -0800

    Recipe properties as lists when needed

--- recipe_compiler/recipe.py
+++ recipe_compiler/recipe.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from .recipe_category import RecipeCategory
+from recipe_compiler.recipe_category import RecipeCategory
+
 import frontmatter
 
 
@@ -10,19 +11,15 @@ class Recipe:
     category: RecipeCategory
     recipe_name: str
     quote: str
-    ingredients: str
-    instructions: str
+    ingredients: list[str]
+    instructions: list[str]
 
-    @staticmethod
-    def from_recipe_text(recipe_text: str) -> "Recipe":
-        recipe_data = frontmatter.loads(recipe_text)
+    @property
+    def slug(self) -> str:
+        """Returns the recipe name formatted as kebab-case
 
-        return Recipe(
-            name=recipe_data["name"],
-            residence=recipe_data["residence"],
-            category=RecipeCategory(recipe_data["category"].lower()),
-            recipe_name="",
-            quote="",
-            ingredients="",
-            instructions="",
-        )
+        Returns:
+            str: The recipe_name in kebab-case format
+        """
+
+        return self.recipe_name.lower().replace(" ", "-")
