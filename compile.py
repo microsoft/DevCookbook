e9d3e6f60627ad22dc86c1f27a7de798d02221b4
theckert <thomas.james.eckert@gmail.com>
2020-11-24 22:38:38 -0800

    Set path_base from target flag

--- compile.py
+++ compile.py
@@ -4,10 +4,18 @@ from recipe_compiler.parse import parse_to_recipe
 from recipe_compiler.render import render_home_page, render_recipe_page
 from recipe_compiler.write import write_home_page, write_recipe_page
 
+import argparse
 import glob
 import os
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-t", "--target", help="The target for compilation ['dev','prod']", required=True
+)
+
 if __name__ == "__main__":
+    target = parser.parse_args().target
+
     # Read
     recipe_files = glob.glob("./recipes/*.md")
     recipe_contents = [read_recipe_file(recipe_file) for recipe_file in recipe_files]
@@ -20,6 +28,11 @@ if __name__ == "__main__":
         loader=PackageLoader("recipe_compiler", "templates"),
         autoescape=select_autoescape(["html"]),
     )
+
+    # Handles the path setting for production versus local
+    # (Production has /DevCookbook/ prepended to the path)
+    env.globals = {"path_base": "/DevCookbook/" if target == "prod" else "/"}
+
     home_page = render_home_page(recipes, env)
     recipe_pages = zip(
         [recipe.slug for recipe in recipes],
